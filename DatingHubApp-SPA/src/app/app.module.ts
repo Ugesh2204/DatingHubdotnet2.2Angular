125149c611ef5e876bc2dd9ec59b6f577d74eef8
ugesh <ugesh.roopchand@hotmail.com>
2019-01-17 12:48:46 +0400

    Alertifyjs Implementation

--- DatingHubApp-SPA/src/app/app.module.ts
+++ DatingHubApp-SPA/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorInterceptorProvider } from './_services/error.interceptors';
+import { AlertifyService } from './_services/alertify.service';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { ErrorInterceptorProvider } from './_services/error.interceptors';
    ],
    providers: [
       AuthService,
-      ErrorInterceptorProvider
+      ErrorInterceptorProvider,
+      AlertifyService
    ],
    bootstrap: [
       AppComponent
